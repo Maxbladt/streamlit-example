a61984157ea839cfce5cd4fd8f9b0d41a20568a3
Maxbladt <99587808+Maxbladt@users.noreply.github.com>
2023-06-25 16:50:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,61 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import os
+import logging
+import time
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#I want logging to be to bot.log
+logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def fill_form(email_address):
+    # Get current directory
+    current_directory = os.getcwd()
+    # Define selenium options
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Create a new instance of the Firefox driver
+    # Here, I assume that chromedriver is in the same directory as your Python script
+    driver = webdriver.Chrome(service=webdriver.chrome.service.Service(executable_path=f'{current_directory}/chromedriver'))
 
-    points_per_turn = total_points / num_turns
+    # rest of the code here...
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+def send_emails(n):
+    # Load the csv
+    df = pd.read_csv('emails.csv')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Initialize counter
+    sent_emails = 0
+
+    for index, row in df.iterrows():
+        # Check if form has been sent (1) or not (0)
+        if row['form_send'] == 0:
+            # Send form
+            fill_form(row['emails'])
+
+            # Update row in DataFrame to indicate form has been sent
+            df.at[index, 'form_send'] = 1
+
+            # Increment counter
+            sent_emails += 1
+
+            # Check if the desired number of emails have been sent
+            if sent_emails >= n:
+                break
+
+    # Save the updated DataFrame back to the CSV
+    df.to_csv('emails.csv', index=False)
+
+# Streamlit code
+st.title('Email Sender')
+
+number_of_emails = st.slider('Select the number of emails to send', 1, 100, 45)
+
+if st.button('Send Emails'):
+    st.write(f'Sending {number_of_emails} emails...')
+    send_emails(number_of_emails)
+    st.write(f'{number_of_emails} emails have been sent.')
